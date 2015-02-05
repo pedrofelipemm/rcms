293abe56e59f63c0c33387cb24fd32508170eba5
Raquel <Raquel@Raquel-PC>
2015-02-05 02:07:56 -0200

    alterações em idiomaMB

--- src/main/java/br/ufscar/rcms/view/mb/IdiomaMB.java
+++ src/main/java/br/ufscar/rcms/view/mb/IdiomaMB.java
@@ -1,59 +1,84 @@
 package br.ufscar.rcms.view.mb;
 
+import java.util.List;
+
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.ViewScoped;
+import javax.faces.model.DataModel;
+import javax.faces.model.ListDataModel;
 
 import br.ufscar.rcms.modelo.entidades.Idioma;
+import br.ufscar.rcms.modelo.entidades.SubAreaAtuacao;
 import br.ufscar.rcms.servico.IdiomaService;
 
 @ViewScoped
 @ManagedBean(name = "idiomaMB")
 public class IdiomaMB extends AbstractMB {
 
-    private static final long serialVersionUID = -7215958678138311243L;
+	private static final long serialVersionUID = -7215958678138311243L;
 
-    @ManagedProperty("#{idiomaService}")
-    private IdiomaService idiomaService;
+	private Idioma idma;
+	private transient DataModel<Idioma> idiomas;
+	private List<Idioma> listaIdiomas;
 
-    private Idioma idioma;
+	@ManagedProperty("#{idiomaService}")
+	private IdiomaService idiomaService;
 
-    @PostConstruct
-    public void inicializar() {
-        setIdioma(new Idioma());
-    }
+	private Idioma idioma;
 
-    public void salvar() {
-    	
-        if (getIdioma() != null) {
-            Idioma idiomaExistente = getIdiomaService().buscarPorDescricao(getIdioma().getDescricao());
+	@PostConstruct
+	public void inicializar() {
 
-            if (idiomaExistente == null) {
-                getIdiomaService().salvar(getIdioma());
-                // TODO i18n
-                adicionarMensagemInfo("Idioma " + idioma.getDescricao() + " cadastrado com sucesso!");
-                idioma = new Idioma();
-            } else {
-                adicionarMensagemAlerta("Já existe um idioma com a descrição: " + idioma.getDescricao());
-            }
+		setIdioma(new Idioma());
+		// carregarDados();
 
-        }
-    }
+	}
 
-    public IdiomaService getIdiomaService() {
-        return idiomaService;
-    }
+	public void salvar() {
 
-    public void setIdiomaService(IdiomaService idiomaService) {
-        this.idiomaService = idiomaService;
-    }
+		if (getIdioma() != null) {
+			Idioma idiomaExistente = (Idioma) getIdiomaService()
+					.buscarPorDescricao(getIdioma().getDescricao());
 
-    public Idioma getIdioma() {
-        return idioma;
-    }
+			if (idiomaExistente == null) {
+				getIdiomaService().salvar(getIdioma());
+				// TODO i18n
+				adicionarMensagemInfo("Idioma " + idioma.getDescricao()
+						+ " cadastrado com sucesso!");
+				idioma = new Idioma();
 
-    public void setIdioma(Idioma idioma) {
-        this.idioma = idioma;
-    }
+			}
+
+			else {
+				adicionarMensagemAlerta("Já existe um idioma com a descrição: "
+						+ idioma.getDescricao());
+			}
+
+			// idiomas = new ListDataModel<Idioma>(idma.getDescricao());
+
+		}
+	}
+
+	// private void carregarDados() {
+	// idm = idiomaService.buscarTodas();
+	//
+	// }
+
+	public IdiomaService getIdiomaService() {
+		return idiomaService;
+	}
+
+	public void setIdiomaService(IdiomaService idiomaService) {
+		this.idiomaService = idiomaService;
+	}
+
+	public Idioma getIdioma() {
+		return idioma;
+	}
+
+	public void setIdioma(Idioma idioma) {
+		this.idioma = idioma;
+	}
 }

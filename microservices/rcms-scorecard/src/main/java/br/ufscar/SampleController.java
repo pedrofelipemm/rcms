6a67adca05bb37305d7cb1349ec776df66287b6c
Pedro Felipe Marques Moreira <pedrofelipemm@gmail.com>
2015-07-09 18:39:14 -0300

    test

--- microservices/rcms-scorecard/src/main/java/br/ufscar/SampleController.java
+++ microservices/rcms-scorecard/src/main/java/br/ufscar/SampleController.java
@@ -1,5 +1,6 @@
 package br.ufscar;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -13,7 +14,11 @@ public class SampleController {
 
     @RequestMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public List<Sample> getSamples() {
-        return Arrays.asList(new Sample(1, "XPTO"), new Sample(2, "ABC"));
+        List<Sample> samples = new ArrayList<Sample>();
+        for (int i = 0; i < 11; i++) {
+            samples.add(new Sample(i, "Sample " + i));
+        }
+        return samples;
     }
 
     @RequestMapping(value = "/1", produces = MediaType.APPLICATION_JSON_VALUE)

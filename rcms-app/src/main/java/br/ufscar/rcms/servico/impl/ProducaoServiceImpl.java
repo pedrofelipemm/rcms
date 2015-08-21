62002e0d37e7ef83c2982180dae0649e8e8900a6
João Antonio Bulgareli <jabulgareli@gmail.com>
2015-08-20 23:56:14 -0300

    Upload do PDF da produção

--- rcms-app/src/main/java/br/ufscar/rcms/servico/impl/ProducaoServiceImpl.java
+++ rcms-app/src/main/java/br/ufscar/rcms/servico/impl/ProducaoServiceImpl.java
@@ -1,13 +1,22 @@
 package br.ufscar.rcms.servico.impl;
 
+import static br.ufscar.rcms.commons.util.FileUtils.generateFileName;
+
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
+import org.apache.commons.io.FileUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import br.ufscar.rcms.dao.ProducaoDAO;
 import br.ufscar.rcms.modelo.entidades.Producao;
+import br.ufscar.rcms.modelo.entidades.TransientFile;
 import br.ufscar.rcms.servico.ProducaoService;
 
 @Service("producaoService")
@@ -15,16 +24,43 @@ import br.ufscar.rcms.servico.ProducaoService;
 public class ProducaoServiceImpl implements ProducaoService {
 
     private static final long serialVersionUID = -3551699743568411854L;
+    private static final Logger LOGGER = LoggerFactory.getLogger(ProducaoServiceImpl.class);
 
     @Autowired
     private ProducaoDAO producaoDAO;
+    
+    @Value("${pasta.arquivos.pdf.producao}")
+    private String pastaArquivos;
 
     @Override
     public void saveOrUpdate(Producao producao) {
+    	
+        salvarArquivo(producao);
+       
         producaoDAO.saveOrUpdate(producao);
     }
 
-    @Override
+    private void salvarArquivo(Producao producao) {
+    	try {
+    		
+            TransientFile arquivo = producao.getArquivoPdf();
+            
+            if(arquivo == null)
+            	return;
+            
+            String fileName = generateFileName(pastaArquivos, producao.getIdProducao().toString(), arquivo.getFileExtension());
+            File file = new File(fileName);
+            producao.setNomePdf(producao.getIdProducao().toString() + arquivo.getFileExtension());
+            
+
+            FileUtils.writeByteArrayToFile(file, arquivo.getFile());
+        } catch (IOException e) {
+            String message = String.format("Erro ao salvar arquivo da produção: %s", producao.getTitulo());
+            LOGGER.error(message, e);
+        }
+	}
+
+	@Override
     public void remove(Producao producao) {
         producaoDAO.remover(producao);
     }

a24477a6a516a017a840096a744899b4f0a30e09
Pedro Felipe Marques Moreira <pedrofelipemm@gmail.com>
2015-09-04 16:44:36 -0300

    ApplicationRefreshEventListener

--- rcms-app/src/main/java/br/ufscar/rcms/view/listener/ApplicationRefreshEventListener.java
+++ rcms-app/src/main/java/br/ufscar/rcms/view/listener/ApplicationRefreshEventListener.java
@@ -1,7 +1,8 @@
-package br.ufscar.rcms.config;
+package br.ufscar.rcms.view.listener;
 
 import static br.ufscar.rcms.commons.util.MiscellanyUtil.isEmpty;
 
+import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,16 +12,19 @@ import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 import br.ufscar.rcms.factory.AutorizacaoFactory;
+import br.ufscar.rcms.factory.ConfiguracaoFactory;
 import br.ufscar.rcms.factory.PesquisadorFactory;
 import br.ufscar.rcms.modelo.entidades.Autorizacao;
+import br.ufscar.rcms.modelo.entidades.Configuracao;
+import br.ufscar.rcms.modelo.entidades.Configuracao.Tipo;
 import br.ufscar.rcms.modelo.entidades.Pesquisador;
 import br.ufscar.rcms.servico.AutorizacaoService;
+import br.ufscar.rcms.servico.ConfiguracaoService;
 import br.ufscar.rcms.servico.PesquisadorService;
 
 @Component
 @Transactional
-// TODO PEDRO RENAME
-public class RcmsServletContextListener implements ApplicationListener<ContextRefreshedEvent> {
+public class ApplicationRefreshEventListener implements ApplicationListener<ContextRefreshedEvent> {
 
     @Autowired
     private AutorizacaoService autorizacaoService;
@@ -28,10 +32,14 @@ public class RcmsServletContextListener implements ApplicationListener<ContextRe
     @Autowired
     private PesquisadorService pesquisadorService;
 
+    @Autowired
+    private ConfiguracaoService configuracaoService;
+
     @Override
     public void onApplicationEvent(final ContextRefreshedEvent event) {
         insertRoles();
         insertResearcher();
+        insertMicroservicesUrls();
     }
 
     private void insertRoles() {
@@ -50,5 +58,16 @@ public class RcmsServletContextListener implements ApplicationListener<ContextRe
 
             pesquisadorService.saveOrUpdate(administrador);
         }
+    }
+
+    private void insertMicroservicesUrls() {
+        Configuracao service1 = ConfiguracaoFactory.createConfiguracao(Tipo.MICROSERVICE_AMOUNT_PRODUCAO_BY_YEAR);
+        Configuracao service2 = ConfiguracaoFactory.createConfiguracao(Tipo.MICROSERVICE_AMOUNT_PRODUCAO_BY_RESEARCHER);
+        List<Configuracao> services = Arrays.asList(service1, service2);
+
+        List<Configuracao> servicesFromDB = configuracaoService.buscarPorTipo(Tipo.MICROSERVICE_AMOUNT_PRODUCAO_BY_YEAR,
+                Tipo.MICROSERVICE_AMOUNT_PRODUCAO_BY_RESEARCHER);
+
+        services.stream().filter(t -> !servicesFromDB.contains(t)).forEach(configuracaoService::saveOrUpdate);
     }
 }
